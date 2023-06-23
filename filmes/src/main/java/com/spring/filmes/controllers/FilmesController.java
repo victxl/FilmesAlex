25ea0977107f59d0ca0ce7931c5fa29adc60fa6e
Victor Henrique <victxl@gmail.com>
2023-06-22 21:30:18 -0300

    Revert "final"
    This reverts commit cb9df3a1d3a5509f99f77b63e423ebb6e79f4b90.

--- filmes/src/main/java/com/spring/filmes/controllers/FilmesController.java
+++ filmes/src/main/java/com/spring/filmes/controllers/FilmesController.java
@@ -2,13 +2,9 @@ package com.spring.filmes.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.spring.filmes.models.Filme;
 import com.spring.filmes.repository.FilmesRepository;
@@ -47,38 +43,6 @@ public class FilmesController {
 		mv.addObject("filmes", filmes);
 		
 		return mv;
-		
-	}
-	
-	@RequestMapping(value="/alterarFilme/{codigoFilme}", method=RequestMethod.GET)
-	public ModelAndView formAlterarFilme(@PathVariable("codigoFilme") long codigoFilme) {
-		
-		Filme filme = fr.findByCodigoFilme(codigoFilme);
-		
-		ModelAndView mv = new ModelAndView("alterar-filme");
-		
-		mv.addObject("filme", filme);
-		
-		return mv;
-		
-	}
-	
-	@RequestMapping(value="/alterarFilme/{codigoFilme}", method=RequestMethod.POST)
-	public String alterarFilme(@Validated Filme filme, BindingResult result, RedirectAttributes attributes) {
-		
-		fr.save(filme);
-		
-		return "redirect:/listarFilmes";
-		
-	}
-	
-	@RequestMapping("/excluirFilme")
-	public String excluirFilme(long codigoFilme) {
-		
-		Filme filme = fr.findByCodigoFilme(codigoFilme);
-		fr.delete(filme);
-		
-		return "redirect:/listarFilmes";
 		
 	}
 	
